f468336f501d338fc832f3cc437e974fae9250ac
Jbur43 <jtburum@gmail.com>
2023-10-03 16:30:54 -0400

    wip. attempting to fix adsbygoogle issue

--- dist/_astro/hoisted.dd4aca48.js
+++
@@ -1,1 +0,0 @@
-import"./GoogleAnalytics.astro_astro_type_script_index_0_lang.6096ba31.js";window.dataLayer=window.dataLayer||[];function a(){dataLayer.push(arguments)}a("js",new Date);a("config","G-2F7W0VSKYS");(window.adsbygoogle||[]).push({});
