81c6e9a62ecd899fa0c252703c02719f2667406b
jtburum <jack.t.burum@gmail.com>
2023-10-24 22:46:02 -0400

    convert all references to tinytechtuts

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,11 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap()],
-  site: "https://devdecks.io"
+  site: "https://tinytechtuts.com",
+  redirects: {
+    '/2023-ecto-trigger-update-without-changeset/': {
+      status: 301,
+      destination: 'https://google.com'
+    }
+  }
 });
