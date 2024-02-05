df592c5d563416bbb603f4ca9518fc8460e18cd6
jtburum <jack.t.burum@gmail.com>
2024-02-04 21:17:07 -0500

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-// export default defineConfig({
-//   integrations: [sitemap()],
-//   site: "https://tinytechtuts.com",
-//   trailingSlash: "never"
-// });
+export default defineConfig({
+  integrations: [sitemap()],
+  site: "https://tinytechtuts.com",
+  trailingSlash: "never"
+});
