835b76c3843c7c3b0341aef34d5093129cdb38b3
jtburum <jack.t.burum@gmail.com>
2024-02-04 21:46:56 -0500

    console

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+console.log("did define config get called?")
 export default defineConfig({
   integrations: [sitemap()],
   site: "https://tinytechtuts.com",
