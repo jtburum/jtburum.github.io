d80725249fa54d69351009ae4b2c8d12e9f2a1df
jtburum <jack.t.burum@gmail.com>
2024-02-04 22:08:57 -0500

    no sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 console.log("did define config get called?")
 export default defineConfig({
-  integrations: [sitemap()],
+  // integrations: [sitemap()],
   site: "https://tinytechtuts.com",
   trailingSlash: "ignore"
 });
