0cc1a276cd0bf19b31188c3436aeee36a89977e6
jtburum <jack.t.burum@gmail.com>
2024-02-04 20:47:00 -0500

    remove trailspace config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap()],
-  site: "https://tinytechtuts.com",
-  trailingSlash: "ignore"
+  site: "https://tinytechtuts.com"
 });
